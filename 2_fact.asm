15f7726748738e590194b9c52ce144e8aac1c6c3
Tushar Adhatrao <40828350+tusharad@users.noreply.github.com>
2022-08-18 21:31:32 +0530

    Add files via upload

---
+++ 2_fact.asm
@@ -0,0 +1,21 @@
+;; Assembly program for factorial of a number
+	section .data
+	msg db "Factorial is %d",10,0
+	section .text
+	extern printf
+	global main
+main:
+	mov ecx,5
+	mov eax,1
+lp:
+	mul ecx
+	cmp ecx,1
+	jz endof
+	dec ecx
+	jmp lp
+endof:
+	push eax
+	push msg
+	call printf
+	add esp,8
+	ret
