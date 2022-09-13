19f5efd580a74750ab2c4ec9deaccae9777713ce
Tushar Adhatrao <40828350+tusharad@users.noreply.github.com>
2022-09-13 22:22:16 +0530

    Update 20_copyStringUsingScasb.asm

--- 20_copyStringUsingScasb.asm
+++ 20_copyStringUsingScasb.asm
@@ -1,16 +1,10 @@
-;;print first and second word from string
+;;print first word from string
 section .data
 	msg db "Low level assembly",10,0
 	len equ $-msg
-section .bss
-	firstWord resb len
-	secondWod resb len
-	firstLen resd 1
-	secondLen resd 1
 section .text
 	global main
 main:
-	mov esi,firstWord
 	mov edi,msg
 	
 	mov al,' '
@@ -26,23 +20,4 @@ main:
 	mov ecx,msg
 	int 0x80
 	
-
-	;; printint first two words that is till first two spaces
-	mov esi,firstWord
-	mov edi,msg
-
-	mov al,' '
-	repnz scasb	;;mov edi forward until al is matched
-	repnz scasb	;;mov edi forward until al is matched
-	
-	mov edx,edi	;;put address of space in edx
-	mov esi,msg	;; put intial address in esi
-	sub edx,esi	;; subtract to get length till space
-
-	;;print first word
-	mov eax,4
-	mov ebx,1
-	mov ecx,msg
-	int 0x80
-
 	ret
